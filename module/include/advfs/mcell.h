7fa266c2b6d4419325c62e6825ed7c36188b943d
Dmitry Sednev <dmitry@sednev.ru>
2026-04-10 01:23:40 +0300

    Add basic sources

---
+++ module/include/advfs/mcell.h
@@ -0,0 +1,12 @@
+//
+// AdvFS-NG: Cross-platform AdvFS for Linux/BSD
+// Copyright (c) 2026, Dmitry Sednev <dmitry@sednev.ru>
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#ifndef __ADVFS_MCELL_H__
+#define __ADVFS_MCELL_H__
+
+#pragma once
+
+#endif // !__ADVFS_MCELL_H__
